8eb213f1021f702b520d996066bcbd4d6f7c98e0
Oli Paul <59258725+OliPaul036202@users.noreply.github.com>
2023-01-02 15:26:35 +0000

    Card Pickup quick fix
    - Can now move across the screen like before but can also be placed back and animations all work fine

--- Assets/Scripts/Card_Scripts/CardHand.cs
+++ Assets/Scripts/Card_Scripts/CardHand.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardHand : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+public class CardHand : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     private Animator animator;
     private bool isPointerDown = false;
+    private bool isHovering = false;
+    private Vector3 cachedPos;
 
     private void Start()
     {
@@ -15,19 +17,26 @@ public class CardHand : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        animator.SetBool("OnHover", true);
+        cachedPos = transform.position;
+        isHovering = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        animator.SetBool("OnHover", false);
+        isHovering = false;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log(this.gameObject.name + " Was Clicked.");
-
+        
         isPointerDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        transform.position = cachedPos;
+        isPointerDown = false;
     }
 
     private void Update()
@@ -37,6 +46,19 @@ public class CardHand : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
             animator.enabled = false;
             Vector3 mousePos = Input.mousePosition;
             transform.position = mousePos;
+        }else
+        {
+            animator.enabled = true;
+            animator.SetBool("OnHover", false);
+        }
+
+        if (isHovering)
+        {
+            animator.SetBool("OnHover", true);
+        }
+        else
+        {
+            animator.SetBool("OnHover", false);
         }
     }
 
